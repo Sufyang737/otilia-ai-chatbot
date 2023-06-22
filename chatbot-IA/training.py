3e43957c3333dd039c497c0d26755a7e682b8568
Sufyang737 <107362900+Sufyang737@users.noreply.github.com>
2023-06-21 23:47:17 -0300

    mejoras y nuevos archivos
    creación de red neuronal y text

--- chatbot-IA/training.py
+++ chatbot-IA/training.py
@@ -2,6 +2,7 @@ import random
 import json
 import pickle
 import numpy as np
+
 import nltk
 from nltk.stem import WordNetLemmatizer
 
@@ -15,12 +16,14 @@ intents = json.loads(open('intents.json').read())
 
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 words = []
 classes = []
 documents = []
 ignore_letters = ['?', '!', '¿', '.', ',']
 
+# Clasifica los patrones y las categorías
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
@@ -35,8 +38,9 @@ words = sorted(set(words))
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
+
 training = []
-output_empty = [0] * len(classes)
+output_empty = [0]*len(classes)
 for document in documents:
     bag = []
     word_patterns = document[0]
@@ -47,10 +51,12 @@ for document in documents:
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training) 
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
 
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
@@ -59,9 +65,9 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+
+sgd = SGD(lr=0.001, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=120, batch_size=5, verbose=1)
 model.save("chatbot_model.h5", train_process)
- 
