5890d89b65e63a677bda5ff910f8905c85aa6da5
Sufyang737 <107362900+Sufyang737@users.noreply.github.com>
2023-06-26 21:43:25 -0300

    subido de archivos
    Conexión a la base de datos

--- chatbot-IA/chatbot.py
+++ chatbot-IA/chatbot.py
@@ -2,6 +2,7 @@ import random
 import json
 import pickle
 import numpy as np
+import mysql.connector
 
 import nltk
 from nltk.stem import WordNetLemmatizer
@@ -32,7 +33,7 @@ def bag_of_words(sentence):
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-    max_index = np.where(res ==np.max(res))[0][0]
+    max_index = np.where(res == np.max(res))[0][0]
     category = classes[max_index]
     return category
 
@@ -40,15 +41,37 @@ def get_response(tag, intents_json):
     list_of_intents = intents_json['intents']
     result = ""
     for i in list_of_intents:
-        if i["tag"]==tag:
+        if i["tag"] == tag:
             result = random.choice(i['responses'])
             break
     return result
 
+# Configurar la conexión a la base de datos
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="databasechatbot"
+)
+
+# Crear un cursor para ejecutar las consultas
+cursor = connection.cursor()
+
+
+# Insertar un registro en la tabla
+def insert_chat_record(user_message, chat_response):
+    sql = "INSERT INTO mensaje_usuario_chat (mensaje_usuario, respuesta_chat) VALUES (%s, %s)"
+    values = (user_message, chat_response)
+    cursor.execute(sql, values)
+    connection.commit()
+    print("Registro insertado correctamente")
+
 while True:
     print("Usuario: ")
-    message=input("")
+    message = input("")
     ints = predict_class(message)
     res = get_response(ints, intents)
-    print("OtiChat: ",res)
-    
+    print("OtiChat: ", res)
+    # Insertar el registro en la base de datos
+    insert_chat_record(message, res)
+
